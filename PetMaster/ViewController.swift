583e0f09187e95a09b89da5753efb1e2b5a00511
Fedor Sychev <VenomSnake13@yandex.ru>
2021-04-10 13:00:17 +0300

    v0.12
    Edited some functions

--- PetMaster/ViewController.swift
+++ PetMaster/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 class ViewController: UIViewController{
     
 
+    @IBOutlet weak var table1Text: UITextField!
+    
+    @IBOutlet var table: [UITableView]!
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        self.tableView .beginUpdates()
+        self.tableView.insertRows(at: [IndexPath.init(row: 0, section: 0)], with: .automatic)
+        self.tableView.endUpdates()
+        
+        
 
         self.view.backgroundColor = UIColor.white
         self.navigationItem.title = "Pets"
